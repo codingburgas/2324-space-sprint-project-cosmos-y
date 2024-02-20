c63411525ca49b379aa3eef0f55cd0f9adc42b34
VNMihaylov22 <132439922+VNMihaylov22@users.noreply.github.com>
2024-02-20 20:14:50 +0200

    Fix info not displayling

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdlib>  // For system("CLS") to clear the console
 #include "./header.h"
+#include "./cosmos_animation.h"
 
-int main() {
+int main() 
+{
     int mainChoice;
     int subChoice;
-
     do {
         displayMainMenu();
 
@@ -22,54 +23,68 @@ int main() {
                 switch (subChoice) {
                 case 1:
                     displayMercuryInfo();
+                    waitForEnter();
                     break;
                 case 2:
                     displayVenusInfo();
+                    waitForEnter();
                     break;
                 case 3:
                     displayEarthInfo();
+                    waitForEnter();
                     break;
                 case 4:
                     displayMarsInfo();
+                    waitForEnter();
                     break;
                 case 5:
                     displayJupiterInfo();
+                    waitForEnter();
                     break;
                 case 6:
                     displaySaturnInfo();
+                    waitForEnter();
                     break;
                 case 7:
                     displayUranusInfo();
+                    waitForEnter();
                     break;
                 case 8:
                     displayNeptuneInfo();
+                    waitForEnter();
                     break;
                 case 9:
                     break;  // Back to Planets Menu
                 default:
                     std::cout << "Invalid choice. Please try again." << std::endl;
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                 }
             } while (subChoice != 9);
             break;
         case 2:
             std::cout << "Galaxies information not implemented in this version." << std::endl;
+            waitForEnter();
             break;
         case 3:
             std::cout << "Other Space Bodies information not implemented in this version." << std::endl;
+            waitForEnter();
             break;
         case 4:
             std::cout << "Exiting Cosmos-Y..." << std::endl;
             break;
         default:
             std::cout << "Invalid choice. Please try again." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
 
         // Wait for user input before clearing the screen
-        std::cout << "Press Enter to continue...";
-        std::cin.ignore();
-        std::cin.get();
+        waitForEnter();
 
     } while (mainChoice != 4);
+
+    
 
     return 0;
 }
--- ConsoleApplication1/ConsoleApplication1/header.h
+++ ConsoleApplication1/ConsoleApplication1/header.h
@@ -37,7 +37,7 @@ void displayMercuryInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "      Mercury       " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the nearest planet to the sun and is also the smallest planet in the solar system." << std::endl;
+    std::cout << "It's the nearest planet to the sun and is also the smallest planet in the solar system." << std::endl;
     std::cout << "Its size is slightly larger than the size of the moon of Earth." << std::endl;
     // Add more details about Mercury
 }
@@ -47,7 +47,7 @@ void displayVenusInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "       Venus        " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "When observed from the Earth, it’s the second brightest and takes 224.7 Earth days to complete one revolution." << std::endl;
+    std::cout << "When observed from the Earth, it's the second brightest and takes 224.7 Earth days to complete one revolution." << std::endl;
     std::cout << "It rotates in the opposite direction to all planets except Uranus." << std::endl;
     // Add more details about Venus
 }
@@ -57,7 +57,7 @@ void displayEarthInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "       Earth        " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the third planet from the Sun and is the only known planet to harbor life." << std::endl;
+    std::cout << "It's the third planet from the Sun and is the only known planet to harbor life." << std::endl;
     std::cout << "The Earth's age is around 4.5 billion years old." << std::endl;
     // Add more details about Earth
 }
@@ -67,7 +67,7 @@ void displayMarsInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "       Mars         " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the fourth planet from the sun and also the second-smallest in the solar system." << std::endl;
+    std::cout << "It's the fourth planet from the sun and also the second-smallest in the solar system." << std::endl;
     std::cout << "Its red color also makes it known as the red planet (presence of iron oxide)." << std::endl;
     // Add more details about Mars
 }
@@ -77,7 +77,7 @@ void displayJupiterInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "      Jupiter       " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the fifth planet from the Sun and the largest planet among all." << std::endl;
+    std::cout << "It's the fifth planet from the Sun and the largest planet among all." << std::endl;
     std::cout << "Jupiter is a giant ball of gases with a one-thousand mass of the Sun." << std::endl;
     // Add more details about Jupiter
 }
@@ -87,17 +87,18 @@ void displaySaturnInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "      Saturn        " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the sixth planet from the Sun and the second-largest planet in the solar system." << std::endl;
-    std::cout << "It’s a giant gas planet with an average radius that is nine times the radius of the Earth." << std::endl;
+    std::cout << "It's the sixth planet from the Sun and the second-largest planet in the solar system." << std::endl;
+    std::cout << "It's a giant gas planet with an average radius that is nine times the radius of the Earth." << std::endl;
     // Add more details about Saturn
 }
+
 
 void displayUranusInfo() {
     system("CLS");
     std::cout << "===================" << std::endl;
     std::cout << "      Uranus        " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the seventh planet from the Sun and has the fourth-largest planet by mass and third-largest by radius." << std::endl;
+    std::cout << "It's the seventh planet from the Sun and has the fourth-largest planet by mass and third-largest by radius." << std::endl;
     std::cout << "It's also referred to as an ice giant." << std::endl;
     // Add more details about Uranus
 }
@@ -107,9 +108,17 @@ void displayNeptuneInfo() {
     std::cout << "===================" << std::endl;
     std::cout << "      Neptune       " << std::endl;
     std::cout << "===================" << std::endl;
-    std::cout << "It’s the eighth planet from the Sun and is the fourth-largest planet by diameter." << std::endl;
+    std::cout << "It's the eighth planet from the Sun and is the fourth-largest planet by diameter." << std::endl;
     std::cout << "The third most massive planet and is the densest giant planet." << std::endl;
     // Add more details about Neptune
+}
+
+
+
+void waitForEnter() {
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
 }
 
 #endif // COSMOS_Y_H
